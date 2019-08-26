3c0c6aca51daddc65ce4b352f00e9afb19412004
sanjay singh <ajutamang10@outlook.com>
2019-08-26 19:54:32 +0545

    ..

--- Bihani/Bihani/urls.py
+++ Bihani/Bihani/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from home.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-   # path('',)
+    
 ]
