216040b8a5132c3a2f8f07b7babdf0e2ecf1c8e5
sanjay singh <ajutamang10@outlook.com>
2019-09-08 17:41:01 +0545

    update

---
+++ other/30_Days_of_Code/Day 2/Car.java
@@ -0,0 +1,52 @@
+public class Car{
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    double weight = 4079; // pounds
+    boolean isTheCarOn = false ;// 0 means false
+    char condition ='A';
+    String nameOfCar = "BMW";
+
+    public void printVariables(){
+        System.out.println("The Name of Car is "+nameOfCar);
+        System.out.println("Maximum Speed of Car is "+maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+        
+    }
+
+    public void wreckCar(){
+        maxSpeed = 200;
+        condition = 'C';
+    }
+    public void upgradeMinSpeed(){
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed+1;
+    }
+
+    public static void main(String[] args){
+        // lets create instance
+        Car familyCar = new Car();
+        System.out.println("Family's Car:");
+        familyCar.printVariables();
+        familyCar.upgradeMinSpeed();
+        familyCar.printVariables();
+
+        // new Car Properties
+        Car AliceCar = familyCar;
+        familyCar.wreckCar();
+        System.out.println("Alice's Car:");
+        AliceCar.printVariables();
+
+    }
+}
+
+/*
+OUTPUT:
+100
+0
+4079.0
+false
+A
+*/
